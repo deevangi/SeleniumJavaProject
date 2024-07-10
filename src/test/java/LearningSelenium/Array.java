7412618b394571da61d78018716fea0e511c19e3
deevangi <deevangibaghela112@gmail.com>
2024-07-10 11:21:36 +0530

    Add Some file

--- src/test/java/LearningSelenium/Array.java
+++ src/test/java/LearningSelenium/Array.java
@@ -5,6 +5,11 @@ public class Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		String[]name= {"rahul","test","selenium"};
+		
+		System.out.println(name[0]);
+
 	}
 
 }
--- src/test/java/LearningSelenium/CoreJava6.java
+++ src/test/java/LearningSelenium/CoreJava6.java
@@ -4,6 +4,40 @@ public class CoreJava6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//String Literal
+		
+		//String s ="test selenium";
+		String s1 ="automation";
+		
+		
+		//new
+		
+		String s2 = new String("Welcome");
+		String s3 = new String ("Welcome");
+		
+		
+		
+		String s ="test selenium project";
+		String[] splittedString = s.split("selenium");
+		
+		System.out.println(splittedString[0]);
+		System.out.println(splittedString[1]);
+		System.out.println(splittedString[1].trim());
+		
+		for(int i=0;i<s.length();i++) {
+			
+			System.out.println(s.charAt(i));
+		}
+		
+		
+		for(int i=s.length()-1;i>=0;i--) {
+			
+			System.out.println(s.charAt(i));
+		}
+
+		
+
 
 	}
 
