8ec27da5597cf5d7e4fb45203276a8011fb3c5a5
deevangi <deevangibaghela112@gmail.com>
2024-09-10 09:48:08 +0530

    Add dropdown code 2

---
+++ src/test/java/LearningSelenium/DropDown1.java
@@ -0,0 +1,50 @@
+package LearningSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import junit.framework.Assert;
+
+public class DropDown1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		/*
+		 * int i=1;
+		 * 
+		 * while(i<5)
+		 * 
+		 * {
+		 * 
+		 * driver.findElement(By.id("hrefIncAdt")).click();
+		 * 
+		 * i++;
+		 * 
+		 * }
+		 */
+
+		for (int i = 1; i < 5; i++)
+
+		{
+
+			driver.findElement(By.id("hrefIncAdt")).click();
+		}
+
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+		driver.findElement(By.id("btnclosepaxoption")).click();
+
+	}
+
+}
