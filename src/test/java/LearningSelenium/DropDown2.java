67185b7b4781fc7aa618130b6ac6d05c7acc299f
deevangi <deevangibaghela112@gmail.com>
2024-09-10 21:12:24 +0530

    Add Dynamic Dropdown code

---
+++ src/test/java/LearningSelenium/DropDown2.java
@@ -0,0 +1,27 @@
+package LearningSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import junit.framework.Assert;
+
+public class DropDown2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+	}
+
+}
