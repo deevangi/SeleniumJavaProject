fdb4ccbb743dd643fc158ed03f87bb5e39e1d294
deevangi <deevangibaghela112@gmail.com>
2024-09-12 09:57:36 +0530

    Add dropdown code dynamic

--- src/test/java/LearningSelenium/DropDown2.java
+++ src/test/java/LearningSelenium/DropDown2.java
@@ -17,10 +17,15 @@ public class DropDown2 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		driver.findElement(By.xpath("//a[@value='BLR']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		//driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		//driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		//Thread.sleep(2000);
+		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+		//Parent and Child path
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+		//driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click();
+		//driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
 	}
 
