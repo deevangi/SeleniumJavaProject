02018b10d98683697ba7e384a9e4c058f5367a70
deevangi <deevangibaghela112@gmail.com>
2024-09-12 20:43:06 +0530

    add checkbox code

---
+++ src/test/java/LearningSelenium/CheckBox.java
@@ -0,0 +1,25 @@
+package LearningSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		// count the number of checkboxes
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
+	}
+
+}
