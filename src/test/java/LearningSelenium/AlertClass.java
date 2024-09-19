b38fe09300fc30f92e9c2dafd159a74fe36ad611
deevangi <deevangibaghela112@gmail.com>
2024-09-19 10:13:19 +0530

    Add Alert class code

---
+++ src/test/java/LearningSelenium/AlertClass.java
@@ -0,0 +1,30 @@
+package LearningSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class AlertClass {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Deevangi";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//Select only ok alert button
+		driver.findElement(By.xpath("//input[@id='name']")).sendKeys(name);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+		
+	}
+
+}
---
+++ src/test/java/LearningSelenium/AlertClass1.java
@@ -0,0 +1,29 @@
+package LearningSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="Deevangi";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		//Select OK and Cancel alert button
+		driver.findElement(By.xpath("//input[@id='name']")).sendKeys(name);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.cssSelector("[id='confirmbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+	}
+
+}
