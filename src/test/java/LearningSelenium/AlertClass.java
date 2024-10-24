86dae88fc25161f7fc7fbc861538bd7cdd22b161
deevangi <deevangibaghela112@gmail.com>
2024-10-24 10:34:05 +0530

    add new project

--- src/test/java/LearningSelenium/AlertClass.java
+++ src/test/java/LearningSelenium/AlertClass.java
@@ -9,22 +9,20 @@ import org.testng.Assert;
 
 public class AlertClass {
 
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name="Deevangi";
+		String name = "Deevangi";
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		//Select only ok alert button
+
+		// Select only ok alert button
 		driver.findElement(By.xpath("//input[@id='name']")).sendKeys(name);
 		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
-		
-		
+
 	}
 
 }
